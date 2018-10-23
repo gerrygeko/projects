7019d28c6dca864cc0f5503152937cef199b06b0
gerrygeko <lamorte.gerardo@gmail.com>
2018-10-23 14:46:06 +0200

    Added more Lombok and created test class for Employee

---
+++ salary-and-taxes/src/test/java/model/EmployeeTest.java
@@ -0,0 +1,33 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void calculateSalaryByMonth() {
+    }
+
+    @Test
+    void calculateSalaryByYear() {
+    }
+
+    @Test
+    void calculateSalaryExtraordinaryMonth() {
+    }
+
+    @Test
+    void getTaxes() {
+    }
+
+    @Test
+    void applyTaxes() {
+    }
+
+}
