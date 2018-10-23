1479532874f18bf506d444d05aff7191e30e9140
gerrygeko <lamorte.gerardo@gmail.com>
2018-10-23 19:25:58 +0200

    Implemented the row extracting and the creation of WorkingDay

--- excel-process/src/main/java/ProcessExcel.java
+++ excel-process/src/main/java/ProcessExcel.java
@@ -1,12 +1,25 @@
+import model.WorkingDay;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class ProcessExcel {
 
 
-    public static final String EXCEL_PROCESS_TEST_XLS = "excel-process\\test.xls";
+    private static final String EXCEL_PROCESS_TEST_XLS = "excel-process\\test.xls";
+    private static final int PAY_RATE_COLUMN = 4;
+    public static final int DAY_INDEX = 0;
+    public static final int START_INDEX = 1;
+    public static final int FINISH_INDEX = 2;
+
+
+    private static DateFormat readFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzzz yyyy");
+    private static DateFormat dayFormat = new SimpleDateFormat("EEE MMM dd yyyy");
+    private static DateFormat hourFormat = new SimpleDateFormat("HH:mm");
 
     public static void main(String[] args) {
 
@@ -17,6 +30,45 @@ public class ProcessExcel {
         Workbook workbook = ExcelFileReader.getWorkbook(pathFile);
         Sheet sheet = workbook.getSheetAt(0);
 
+        Map<Integer, List<String>> mapOfRows = getRowsMap(sheet);
+
+        //Code that need deleted. We are going to define a class that describe the sheet that has been loaded from the Excel.
+        //This is just a test to print the content of the file.
+        Iterator it = mapOfRows.entrySet().iterator();
+        float payRate = getPayRate(it); //get the first row and extract the pay rate
+        System.out.println("Pay Rate for hour is: " + payRate);
+        while(it.hasNext()) {
+            WorkingDay workingDay = extractRow(it);
+            System.out.println(workingDay.toString());
+        }
+    }
+
+    private static WorkingDay extractRow(Iterator it) {
+
+        WorkingDay workingDay = null;
+
+        Map.Entry pair = (Map.Entry) it.next();
+        ArrayList<String> list = (ArrayList<String>) pair.getValue();
+
+        try {
+            Date day = readFormat.parse(list.get(DAY_INDEX));
+            Date startTime = readFormat.parse(list.get(START_INDEX));
+            Date finishTime = readFormat.parse(list.get(FINISH_INDEX));
+
+            String parsedDay = dayFormat.format(day);
+            String parsedStartTime = hourFormat.format(startTime);
+            String parsedFinishTime = hourFormat.format(finishTime);
+
+            workingDay = new WorkingDay(dayFormat.parse(parsedDay), hourFormat.parse(parsedStartTime), hourFormat.parse(parsedFinishTime));
+            workingDay.toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return workingDay;
+    }
+
+    private static Map<Integer, List<String>> getRowsMap(Sheet sheet) {
         Map<Integer, List<String>> data = new HashMap<>();
         int i = 0;
         for (Row row : sheet) {
@@ -47,23 +99,14 @@ public class ProcessExcel {
             i++;
 
         }
+        return data;
+    }
 
-        //Code that need deleted. We are going to define a class that describe the sheet that has been loaded from the Excel.
-        //This is just a test to print the content of the file.
-
-        Iterator it = data.entrySet().iterator();
-        while(it.hasNext()) {
-            StringBuilder sb = new StringBuilder();
-            Map.Entry pair = (Map.Entry) it.next();
-
-            sb.append("Row: " + pair.getKey() + "\n");
-            ArrayList<String> list = (ArrayList<String>) pair.getValue();
-            for(String value : list) {
-                sb.append(value + " | ");
-            }
-            System.out.println(sb.toString());
-        }
-
+    private static float getPayRate(Iterator it) {
+        Map.Entry pair = (Map.Entry) it.next();
+        ArrayList<String> row = (ArrayList<String>) pair.getValue();
+        float payRate = Float.parseFloat(row.get(PAY_RATE_COLUMN));
+        return payRate;
     }
 
 }
--- excel-process/src/main/java/model/WorkingDay.java
+++ excel-process/src/main/java/model/WorkingDay.java
@@ -2,6 +2,7 @@ package model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -9,12 +10,25 @@ import java.util.Date;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class WorkingDay {
 
-    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    private final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     private Date day;
     private Date startTime;
     private Date finishTime;
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Working day: ");
+        sb.append(day + "\n");
+        sb.append("Starting time: ");
+        sb.append(startTime + "\t");
+        sb.append("Finishing time: ");
+        sb.append(finishTime + "\n");
+        return sb.toString();
+    }
 }
